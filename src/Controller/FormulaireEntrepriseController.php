34688ae0e2362b8cb40eccf9d42f92864a438e87
Mateo Fialoux <fialouxmateo@gmail.com>
2025-12-12 16:41:17 +0100

    Formulaire non complet Entreprise

--- src/Controller/FormulaireEntrepriseController.php
+++ src/Controller/FormulaireEntrepriseController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Form\FormulaireEntrepriseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FormulaireEntrepriseController extends AbstractController
 {
     #[Route('/formentreprise', name: 'app_formulaire_entreprise')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createForm(FormulaireEntrepriseType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entreprise = $form->getData();
+        }
+
         return $this->render('formulaire_entreprise/index.html.twig', [
-            'controller_name' => 'FormulaireEntrepriseController',
+            'form' => $form->createView(),
         ]);
     }
 }
