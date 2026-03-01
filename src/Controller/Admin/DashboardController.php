07a0d0ad588d231c89085fa8297410e830c77c48
Mateo Fialoux <fialouxmateo@gmail.com>
2026-03-01 14:02:12 +0100

    Dashboard Admin pour secrétaire

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -12,7 +12,7 @@ use App\Entity\User;
 use App\Entity\Entreprise;
 use App\Entity\Log;
 
-#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_SECRETAIRE')]
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
@@ -35,8 +35,12 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
-        yield MenuItem::linkToCrud('Entreprise', 'fas fa-list', Entreprise::class);
-        yield MenuItem::linkToCrud('Log', 'fas fa-list', Log::class);
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        // Seulement pour ADMIN
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToCrud('Entreprise', 'fas fa-list', Entreprise::class);
+            yield MenuItem::linkToCrud('Log', 'fas fa-list', Log::class);
+        }
     }
+
 }
